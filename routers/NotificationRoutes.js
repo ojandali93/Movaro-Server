0aad6314e02c9e629a477131ff25a104f4fe8b9b
omar jandali <omarjandali93@gmail.com>
2025-07-02 14:54:16 -0700

    omar jandali: updated serve r

--- routers/NotificationRoutes.js
+++ routers/NotificationRoutes.js
@@ -1,7 +1,30 @@
 import { sendPush } from '../utils/apn.js';
 import { Router } from 'express';
+import { supabase } from '../utils/SupabaseClient.js';
 
 const router = Router();
+
+router.post('/store-device-token', async (req, res) => {
+  const { userId, token } = req.body;
+
+  console.log(userId, token);
+
+  if (!userId) {
+    return res.status(400).json({ error: 'Missing userId' });
+  }
+
+  const { error } = await supabase
+    .from('Profile') // Replace with your actual table
+    .update({ apnToken: token })
+    .eq('userId', userId);
+
+  if (error) {
+    console.error('❌ Failed to store APNs token:', error.message);
+    return res.status(500).json({ error: 'Error storing token' });
+  }
+
+  return res.status(200).json({ success: true });
+});
 
 // Trigger push
 router.post('/send', async (req, res) => {
